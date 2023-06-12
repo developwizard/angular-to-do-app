4cc655caad659cbe8398776fc24fa34c111a2680
dewiz <churkin.it@gmail.com>
2023-06-12 23:56:39 +0300

    Added todo task template

--- src/app/todo/todo.component.ts
+++ src/app/todo/todo.component.ts
@@ -1,4 +1,10 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {MatCardModule} from "@angular/material/card";
+import {MatFormFieldModule} from "@angular/material/form-field";
+import {MatInputModule} from "@angular/material/input";
+import {MatIconModule} from "@angular/material/icon";
+import {MatButtonModule} from "@angular/material/button";
+import {FormBuilder, FormGroup, ReactiveFormsModule, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-todo',
@@ -6,9 +12,23 @@ import {Component} from '@angular/core';
   styleUrls: ['./todo.component.css'],
   standalone: true,
   imports: [
-
+    MatButtonModule,
+    MatCardModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatIconModule,
+    ReactiveFormsModule
   ]
 })
-export class TodoComponent {
+export class TodoComponent implements OnInit {
+  todoForm!: FormGroup;
 
+  constructor(private fb: FormBuilder) {
+  }
+
+  ngOnInit(): void {
+    this.todoForm = this.fb.group({
+      item: ['', Validators.required]
+    });
+  }
 }
